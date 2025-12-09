34f5f3708920d249b99f68d52a7cd7867a85e42e
Amélie <amelie.beaufils95@gmail.com>
2025-12-09 11:50:33 +0100

    fix: request

---
+++ src/composants/Grid.tsx
@@ -0,0 +1,9 @@
+import { useNavigate } from 'react-router-dom';
+
+export function Grid{
+    return(
+        <>
+        <h1>Grid</h1>
+        </>
+    );
+}
---
+++ src/composants/Score.tsx
@@ -0,0 +1,9 @@
+import { useNavigate } from 'react-router-dom';
+
+export function Score{
+    return(
+        <>
+        <h1>Score</h1>
+        </>
+    );
+}
