14e18442044aeee0e3a9da835818f71a225442d7
Amélie <amelie.beaufils95@gmail.com>
2025-12-10 09:26:20 +0100

    feat: intro game

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,22 @@
-import { useNavigate } from 'react-router-dom';
-import { Button } from '../components/Button';
+import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
 
 export function Home() {
   const navigate = useNavigate();
 
   const handleStartGame = () => {
-    navigate('/game');
+    navigate("/game");
   };
 
   return (
     <div className="bg-slate-700 text-white flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-4xl font-bold">Maze Game</h1>
       <p className="text-gray-400">Welcome to the Maze Game!</p>
+      <p>
+        The goal of the game is to find the exit by lifting the tiles to reveal
+        the path, you can only reveal tiles adjacent to the one already
+        revealed.
+      </p>
       <Button onClick={handleStartGame}>Start Game</Button>
     </div>
   );
