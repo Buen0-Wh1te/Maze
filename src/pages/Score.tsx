a7e5698ff64eb158c5f49e7f29c983ce8a2fbe16
Corentin Renard <corentinfox08@gmail.com>
2025-12-10 23:52:00 +0100

    style: make score background solid black

--- src/pages/Score.tsx
+++ src/pages/Score.tsx
@@ -94,7 +94,7 @@ export function Score() {
       </motion.div>
       {score && (
         <motion.div
-          className="bg-black/60 px-4 py-2 rounded text-sm mt-6"
+          className="bg-black px-4 py-2 rounded text-sm mt-6"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.5, duration: 0.5 }}
