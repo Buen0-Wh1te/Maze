58dbcbf17306ae163d1863c99fda11845c2ab55e
Corentin Renard <corentinfox08@gmail.com>
2025-12-10 12:15:13 +0100

    Init/music (#46)
    * feat: added background music
    
    added button
    
    * feat: add cursor styles to button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GameProvider } from './context/GameProvider';
+import { BackgroundMusic } from './components/BackgroundMusic';
 import { Home } from './pages/Home';
 import { Game } from './pages/Game';
 import { Score } from './pages/Score';
+import backgroundMusic from './assets/musics/background.mp3';
 
 function App() {
   return (
     <GameProvider>
+      <BackgroundMusic src={backgroundMusic} volume={0.4} />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
