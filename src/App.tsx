9940409990a628923bfc2a4326116ed01a70907d
Corentin Renard <corentinfox08@gmail.com>
2025-12-10 10:18:50 +0100

    feat: add pseudo input with storage (#43)
    * feat: add pseudo input field on home page
    
    Closes #9
    
    * feat: add GameContext for pseudo storage
    
    * fix: separate context, provider, and hook into individual files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { GameProvider } from './context/GameProvider';
 import { Home } from './pages/Home';
 import { Game } from './pages/Game';
 import { Score } from './pages/Score';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/score" element={<Score />} />
-      </Routes>
-    </BrowserRouter>
+    <GameProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/score" element={<Score />} />
+        </Routes>
+      </BrowserRouter>
+    </GameProvider>
   );
 }
 
