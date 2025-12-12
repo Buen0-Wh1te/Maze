6f7e7804aae75273daeaab04e414e34acc733b0b
Corentin Renard <corentinfox08@gmail.com>
2025-12-12 01:20:01 +0100

    feat: increase animation speed (50ms run, 120ms idle)

--- src/components/PlayerSprite.tsx
+++ src/components/PlayerSprite.tsx
@@ -32,7 +32,7 @@ export function PlayerSprite({
 
   // Animate sprite frames
   useEffect(() => {
-    const frameDelay = isMoving ? 100 : 150; // Faster animation when moving
+    const frameDelay = isMoving ? 50 : 120; // Very fast animation when moving
 
     const interval = setInterval(() => {
       setCurrentFrame((prev) => (prev + 1) % animation.frames);
